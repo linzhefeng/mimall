2608c478438a7134e610b63f59a09fa8d57ad442
linzhefeng <156841569@qq.com>
2021-01-26 23:47:03 +0800

    目录结构 构建完成

---
+++ src/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+import NavFooter from '../components/NavFooter.vue'
+import NavHeader from '../components/NavHeader.vue'
+export default {
+    data() {
+        return {}
+    },
+    methods: {},
+    components: { NavHeader, NavFooter }
+}
+</script>
+<style></style>
