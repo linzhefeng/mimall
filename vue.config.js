751e623075fdf55eae2b10ae07627885f23aada4
linzhefeng <156841569@qq.com>
2021-03-21 21:52:08 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,13 @@ module.exports = {
                 }
             }
         }
+    },
+    // publicPath:'/app',
+    // outputDir:'dist',
+    // indexPath:'index2.html',
+    // lintOnSave:false,
+    productionSourceMap: false,
+    chainWebpack: config => {
+        config.plugins.delete('prefetch')
     }
 }
